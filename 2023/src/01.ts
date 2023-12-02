137710891152e34eb7d204d84960bec6dfd1efb7
Mike Cornell <mcornell74@gmail.com>
2023-12-02 08:36:45 -0500

    just words work

--- 2023/src/01.ts
+++ 2023/src/01.ts
@@ -6,9 +6,44 @@ export function find_code(calibration_line: string): number {
         }
     }
     let code = numbers?.shift() ?? "0";
-    let code_end =  numbers?.pop() ?? code;
+    let code_end = numbers?.pop() ?? code;
 
     return Number(code + code_end);
+}
+
+export function find_word_codes(calibration_line: string): number {
+
+    const numbers: string[] = ["zero","one","two","three","four","five","six","seven","eight","nine"];
+    
+    console.log(numbers);
+    let first_number_index = calibration_line.length;
+    let first_number_string = "NaN";
+    for (let num of numbers) {
+        console.log("looking for " +num);
+        const index = calibration_line.indexOf(num);
+        if (index > -1 && index < first_number_index) {
+            first_number_index = index;
+            first_number_string = num;
+            console.log('found ' + num + " at " + index);
+        }
+    }
+    const first_number = numbers.indexOf(first_number_string);
+    console.log(first_number);
+
+    // find back LastIndexOf
+    let last_number_index = 0;
+    let last_number_string = "NaN";
+    for (let num of numbers) {
+        const index = calibration_line.lastIndexOf(num);
+        if (index > -1 && index > last_number_index) {
+            last_number_index = index;
+            last_number_string = num;
+            console.log('found ' + num + " at " + index);
+        }
+    }
+    const last_number = numbers.indexOf(last_number_string);
+
+    return first_number * 10 + last_number;
 }
 
 export function process_calibration(lines: string[]): number {
--- 2023/test/01.test.ts
+++ 2023/test/01.test.ts
@@ -1,4 +1,4 @@
-import { find_code, process_calibration } from '../src/01';
+import { find_code, find_word_codes, process_calibration } from '../src/01';
 import { read_file } from '../src/utils';
 
 describe('test find code', () => {
@@ -31,3 +31,33 @@ describe('test process calibration', () => {
         expect(process_calibration(test_lines)).toBe(55538);
     });
 });
+
+describe('test find word codes', ()  => {
+    test('example one', () => {
+        expect(find_word_codes('two1nine')).toBe(29);
+    });
+    test('example two', () => {
+        expect(find_word_codes('eightwothree')).toBe(83);
+    });
+    test('example 3', () => {
+        expect(find_word_codes('abcone2threexyz')).toBe(13);
+    });
+    test('example 4', () => {
+        expect(find_word_codes('xtwone3four')).toBe(24);
+    });
+    test('example 5', () => {
+        expect(find_word_codes('4nineeightseven2')).toBe(42);
+    });
+    test('example 6', () => {
+        expect(find_word_codes('two1ninzoneight234e')).toBe(14);
+    });
+    test('example 7', () => {
+        expect(find_word_codes('7pqrstsixteen')).toBe(76);
+    });
+});
+
+
+
+
+
+
