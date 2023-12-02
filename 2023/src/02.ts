9f290e887ab5b2024d3bb5a06278f0d1a51721a7
Mike Cornell <mcornell74@gmail.com>
2023-12-02 18:56:19 -0500

    day 2 done

--- 2023/src/02.ts
+++ 2023/src/02.ts
@@ -1,3 +1,5 @@
+import { freemem } from "os";
+
 export function is_game_possible(game_string: string, bag_contents: Map<string, number> = new Map<string, number>([["red", 12], ["green", 13], ["blue", 14]])): boolean {
 
     const rounds = game_string.split(';');
@@ -24,6 +26,34 @@ export function process_games(game_lines: string[], bag_contents: Map<string, nu
         if (is_game_possible(game_info[1].trim(), bag_contents)) {
             total += +game_number;
         }
+    }
+    return total;
+}
+
+export function find_fewest_set(game_string: string): Map<string,number> {
+    let fewest: Map<string,number> = new Map<string,number>([["red", 0], ["green", 0], ["blue", 0]]);
+    const rounds = game_string.split(';');
+    for (const round of rounds) {
+        const cubes = round.trim().split(',');
+        for (const cube of cubes) {
+            const info = cube.trim().split(' ');
+            const count: number = +info[0];
+            const fewest_color = fewest.get(info[1]) ?? 0;
+            if (count > fewest_color) {
+                fewest.set(info[1], count);
+            }
+        }
+    }
+    return fewest;
+}
+
+export function process_power_games(game_lines: string[]): number {
+    let total = 0;
+    for (const line of game_lines) {
+        const game_info: string[]= line.split(":");
+        const fewest = find_fewest_set(game_info[1].trim());
+        const game_product = [...fewest.values()].reduce((product, count) => product * count);
+        total += game_product;
     }
     return total;
 }
--- 2023/test/02.test.ts
+++ 2023/test/02.test.ts
@@ -1,5 +1,5 @@
 import { read_file } from '../src/utils';
-import { is_game_possible, process_games } from '../src/02';
+import { is_game_possible, process_games, find_fewest_set, process_power_games } from '../src/02';
 
 describe('test find code', () => {
     test('example one', () => {
@@ -27,5 +27,35 @@ describe('test process games', () =>{
     test('puzzle 1', () => {
         let test_lines = read_file('./test/puzzle-2-1.txt');
         expect(process_games(test_lines)).toBe(3099);
+    });
+});
+
+describe('test find fewest set', () => {
+    test('example 1', () => {
+        expect(find_fewest_set('3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green')).toEqual(new Map<string, number>([["red", 4], ["green", 2], ["blue", 6]]));
+    });
+    test('example 2', () => {
+        expect(find_fewest_set('1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue')).toEqual(new Map<string, number>([["red", 1], ["green", 3], ["blue", 4]]));
+    });
+    test('example 3', () => {
+        expect(find_fewest_set('8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red')).toEqual(new Map<string, number>([["red", 20], ["green", 13], ["blue", 6]]));
+    });
+    test('example 4', () => {
+        expect(find_fewest_set('1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red')).toEqual(new Map<string, number>([["red", 14], ["green", 3], ["blue", 15]]));
+    });
+    test('example 5', () => {
+        expect(find_fewest_set('6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green')).toEqual(new Map<string, number>([["red", 6], ["green", 3], ["blue", 2]]));
+    });
+
+});
+
+describe('test process games power', () =>{
+    test('example 1', () => {
+        let test_lines = read_file('./test/test-2-1.txt');
+        expect(process_power_games(test_lines)).toBe(2286);
+    });
+    test('puzzle 1', () => {
+        let test_lines = read_file('./test/puzzle-2-1.txt');
+        expect(process_power_games(test_lines)).toBe(72970);
     });
 });
